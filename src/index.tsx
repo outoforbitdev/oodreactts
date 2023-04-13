7ead5fcb9da31be701df6ec5855e198b50388491
Jay Mirecki <jarett.mirecki@gmail.com>
2023-04-13 17:09:39 -0500

    feat(theme): export theme functionality

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,7 @@ import InfoboxSection from "./components/infobox/InfoBoxSection";
 import NavBar from "./components/navBar/NavBar";
 import NavBarText from "./components/navBar/NavBarText";
 import NavBarButton from "./components/navBar/NavBarButton";
+import { setTheme } from "./utils/webpage/setTheme";
+import { Themes } from "./utils/webpage/setTheme";
 
-export { Infobox, InfoboxLine, InfoboxSection, NavBar, NavBarText, NavBarButton };
+export { Infobox, InfoboxLine, InfoboxSection, NavBar, NavBarText, NavBarButton, setTheme, Themes };
